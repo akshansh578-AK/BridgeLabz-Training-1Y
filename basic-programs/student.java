28f60ebd480df1fce531a56cd7712e0eca0232ac
akshansh578-AK <akshansh578@gmail.com>
2026-01-19 21:52:51 +0530

    Updated basic programs with multiple folders

---
+++ basic-programs/student.java
@@ -0,0 +1,13 @@
+class student {
+    int id;
+    String name;
+    int age;
+
+   
+    void displayDetails() {
+        System.out.println("Student ID   : " + id);
+        System.out.println("Student Name : " + name);
+        System.out.println("Student Age  : " + age);
+    }
+}
+

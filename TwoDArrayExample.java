74f5e9669ed8d5a104e59c0a2701cb00ec203e0f
akshansh578-AK <akshansh578@gmail.com>
2026-02-10 14:25:02 +0530

    Class Work

--- TwoDArrayExample.java
+++ TwoDArrayExample.java
@@ -1,16 +1,14 @@
-package BASICS_OF_JAVA;
-
-public class TwoDArrayExample {
-    public static void main(String[] args){
-        int [][] marks = {
-                {80, 90},
-                {70, 85}
-        };
-        for (int i = 0; i<marks.length; i++){
-            for(int j =0 ; j< marks[i].length; j++) {
-                System.out.println(marks[i][j] +" ");
-            }
-            System.out.println();
-        }
-    }
-}
+class TwoDArrayExample{
+public static void main(String[]args){
+int[][] marks={
+       {7, 9},
+	   {5, 5}
+	   };
+	   for(int i=0;i<marks.length; i++){
+	   for(int j=0;j<marks[i].length; j++){
+	   System.out.println(marks[i][j]+ " ");
+	   }
+	   System.out.println();
+	   }
+	   }
+	   }
--- calculator.java
+++ calculator.java
@@ -1,54 +1,19 @@
 import java.util.Scanner;
-
-public class Calculator {
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Simple Calculator");
-        System.out.println("------------------");
-
-        System.out.print("Enter first number: ");
-        double num1 = sc.nextDouble();
-
-        System.out.print("Enter second number: ");
-        double num2 = sc.nextDouble();
-
-        System.out.println("\nChoose an operation:");
-        System.out.println("1. Addition (+)");
-        System.out.println("2. Subtraction (-)");
-        System.out.println("3. Multiplication (*)");
-        System.out.println("4. Division (/)");
-
-        System.out.print("Enter your choice (1-4): ");
-        int choice = sc.nextInt();
-
-        switch(choice) {
-
-            case 1:
-                System.out.println("Result: " + (num1 + num2));
-                break;
-
-            case 2:
-                System.out.println("Result: " + (num1 - num2));
-                break;
-
-            case 3:
-                System.out.println("Result: " + (num1 * num2));
-                break;
-
-            case 4:
-                if(num2 != 0) {
-                    System.out.println("Result: " + (num1 / num2));
-                } else {
-                    System.out.println("Error! Division by zero is not allowed.");
-                }
-                break;
-
-            default:
-                System.out.println("Invalid choice!");
-        }
-
-        sc.close();
-    }
+class Calculator{
+public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the first number");
+int a=sc.nextInt();
+System.out.println("enter the second number");
+int b= sc.nextInt();
+int c=a+b;
+int d= a-b;
+int e=a*b;
+double f=a/b;
+System.out.println("the sum is "+c);
+System.out.println("the difference is "+d);
+System.out.println("the product  is "+e);
+System.out.println("the division is "+f);
 }
+}
+ 

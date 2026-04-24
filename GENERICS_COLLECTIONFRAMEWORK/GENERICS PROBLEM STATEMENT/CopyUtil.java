0683a678c7aeaeeeb9dab5fff50a6df8879f36fe
Akshansh Mishra <akshansh578@gmail.com>
2026-04-24 22:44:29 +0530

    Delete GENERICS_COLLECTIONFRAMEWORK directory

--- GENERICS_COLLECTIONFRAMEWORK/GENERICS PROBLEM STATEMENT/CopyUtil.java
+++
@@ -1,7 +0,0 @@
-class CopyUtil {
-    public static void copyList(List<? super Number> dest, List<? extends Number> src) {
-        for (Number n : src) {
-            dest.add(n);
-        }
-    }
-}
